47a6cd221e6fe8706c378b19ff9d863a36d36092
MarianaOrtegaRam <m.ortegar@uniandes.edu.co>
2024-08-21 12:12:20 -0500

    version sin while

--- src/Archivo.java
+++ src/Archivo.java
@@ -1,34 +1,54 @@
 public class Archivo {
 
+
+    //ejeuctando
+    //dormido
+    //listo para ejecutar
     private int nRedactores = 0;
     private int nLectores = 0;
+
+    /*
+    en bloquyes sincronos hay exclusion mutua, pero cuando entra a uyn wait
+     el thread queda dormido, y libera el monitor y deja cualquier thread a entrar a los metodos sincronos
+    */
+
 
     /*
     Debo crear procedemiento que entre a escribir o entré a leer
     */
 
-    public synchronized  void entroEscribir() throws InterruptedException{
+    public synchronized  void entroEscribir(){
         /*
         Se que puedo usar el recurso cuando hay 0 lectores
         */
         if ((nLectores > 0) || (nRedactores > 0)){
-            wait();
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         nRedactores++;
         //aca se pueden quedar esperando mucho tiempo 
 
     }
 
-    public synchronized  void entroLeer() throws InterruptedException{
+    public synchronized  void entroLeer() {
         if(nRedactores > 0){
-            wait();
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         nLectores++;
         //aca son educaditos y van entrando lento, es como si hay un guardia 
         
     }
 
-    public synchronized  void salirEscribir() throws InterruptedException{
+    public synchronized  void salirEscribir() {
         
         nRedactores--;
         // podemos despertar a cualquiera entonces hacenos la siguiente instruccion
@@ -37,11 +57,24 @@ public class Archivo {
     }
 
     
-    public synchronized  void salirLeer() throws InterruptedException{
+    public synchronized  void salirLeer() {
         
         nLectores--;
         // si hay quedan cero lectores, es decir acaba de salir el ultimo, solo hago notify porque soloo quedan redactores,y esos entran de a uno
         notify();
+        
+    }
+
+    public void escribir(String texto){
+    /*
+    Cuando entra aca, es porque uno está escribiendo y los demas están obligatoriamente dormidos, entonces nadie va apoder entrar
+    por el wait (linea 25 )
+    */
+     //TODO DE LO QUE SE HACE PARA CAMBIAR EL ARCHIVO ... 
+
+    }
+
+    public void leer(){
         
     }
 }
